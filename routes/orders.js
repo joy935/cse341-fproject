24030ad4027485a0a0f492861ef7c11de4f522d9
joy935 <tupaiv@gmail.com>
2024-11-28 10:34:45 +0100

    added orders route

---
+++ routes/orders.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const ordersController = require('../controllers/orders');
+
+router.get('/', ordersController.getOrders);
+
+module.exports = router;
