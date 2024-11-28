ce7dc29b28aeeb542b965acd99daf4a21a063177
joy935 <tupaiv@gmail.com>
2024-11-28 10:34:31 +0100

    accessed db

---
+++ controllers/orders.js
@@ -0,0 +1,17 @@
+const mongodb = require('../data/database');
+
+const getOrders = async (req, res) => {
+    try {
+        const result = await mongodb.getDb().db().collection('order').find();
+        result.toArray().then((orders) => {
+            res.setHeader('Content-Type", "application/json');
+            res.status(200).json(orders);
+        });
+    } catch (error) {
+        res.status(500).json(error || 'Some error occurred while fetching the orders.');
+    }
+};
+
+module.exports = {
+    getOrders
+};
