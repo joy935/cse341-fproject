1c24a3aaf6d8f7a6d18958609438ca40e0f4da43
joy935 <tupaiv@gmail.com>
2024-12-11 12:48:40 +0100

    run prettier

--- swagger.js
+++ swagger.js
@@ -1,16 +1,17 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
-    info: {
-      title: 'CSE341 Final Project - Fall 2024: Book Smart API',
-      description: 'This is a group project created as part of the BYU-Idaho class requirements for CSE341: Web Services.'
-    },
-    host: 'booksmart-ogca.onrender.com',
-    schemes: ['https']
+  info: {
+    title: "CSE341 Final Project - Fall 2024: Book Smart API",
+    description:
+      "This is a group project created as part of the BYU-Idaho class requirements for CSE341: Web Services.",
+  },
+  host: "booksmart-ogca.onrender.com",
+  schemes: ["https"],
 };
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index'];
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes/index"];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
