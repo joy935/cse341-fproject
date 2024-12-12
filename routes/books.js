198dea6085736a2d233a5b8c6c688d1def98cb9c
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2024-12-12 00:27:13 +0000

    Adding OAuth  github authentication

--- routes/books.js
+++ routes/books.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 const validation = require("../middleware/validate");
 const booksController = require("../controllers/books");
+const { isAuthenticated } = require("../middleware/authenticate");
 
 router.get("/", booksController.getBooks);
 router.get("/:id", booksController.getBook);
-router.post("/", validation.saveBook, booksController.createBook);
-router.put("/:id", validation.saveBook, booksController.updateBook);
-router.delete("/:id", booksController.deleteBook);
+router.post(
+  "/",
+  isAuthenticated,
+  validation.saveBook,
+  booksController.createBook
+);
+router.put(
+  "/:id",
+  isAuthenticated,
+  validation.saveBook,
+  booksController.updateBook
+);
+router.delete("/:id", isAuthenticated, booksController.deleteBook);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,81 @@
 const express = require("express");
 const app = express();
 const mongodb = require("./data/database");
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
+const cors = require("cors");
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-
 const routes = require("./routes");
-app.use("/", routes);
+
+app
+  .use(express.json())
+  .use(
+    session({
+      // this is the basic express session({...}) initialization.
+      secret: "secret",
+      resave: false,
+      saveUnitialized: true,
+    })
+  )
+  .use(passport.initialize()) //Init passport in every route call
+  .use(passport.session()) // Allow passport to use "express-session"
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requeted-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
+  })
+  .use(cors({ methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"] }))
+  .use(cors({ rigin: "*" }))
+  .use("/", routes);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName || req.session.user.username}`
+      : "BookSmart API. You are logged out"
+  );
+});
+
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "api-docs",
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect("/");
+  }
+);
 
 process.on("uncaughtException", (err, origin) => {
   console.log(
